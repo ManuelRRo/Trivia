a01861369678d486f92e3618a1f1524375670097
Manuel Rico <ds23906@gmail.com>
2020-11-15 16:59:39 -0600

    topic,answer,question container added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const btnSubmit = document.getElementById("btnSubmit");
+const loginContainer = document.getElementsById("login");
+const answerContainer = document.getElementsById("answers");
+const questionContainer = document.getElementById("questions");
+
+btnSubmit.addEventListener("Click",function () {
+    
+    
+});
