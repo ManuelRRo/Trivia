489acbef153a30ebd0a16f1e310e5a6628dd1566
Manuel Rico <ds23906@gmail.com>
2020-11-18 15:19:55 -0600

    First transition screen done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 const btnSubmit = document.getElementById("btnSubmit");
-const loginContainer = document.getElementsById("login");
-const answerContainer = document.getElementsById("answers");
+const loginContainer = document.getElementById("login");
+const answerContainer = document.getElementById("answers");
 const questionContainer = document.getElementById("questions");
+const header = document.getElementById("header_");
+const txtNickName = document.getElementById("nickname");
+const topicGameContainer = document.getElementById("topicGame");
 
-btnSubmit.addEventListener("Click",function () {
-    
-    
-});
+let nameClasses = ["showElement"];
+let nickName = "";
+
+WelcomeScreen(nameClasses[0], "add");
+
+btnSubmit.addEventListener("click", function () {
+    WelcomeScreen(nameClasses[0], "remove");
+    TopicGameScreen(nameClasses[0],"add");
+})
+
+function WelcomeScreen(nameClass, action_) {
+    if (action_ == "remove") {
+        header.classList.remove(nameClass);
+        loginContainer.classList.remove(nameClass);
+    } else {
+        header.classList.add(nameClass);
+        loginContainer.classList.add(nameClass);
+    }
+}
+
+function TopicGameScreen(nameClass, action_) {
+    if (action_ === "remove") {
+        topicGameContainer.classList.remove(nameClass);
+    } else {
+        topicGameContainer.classList.add(nameClass);
+    }
+}
