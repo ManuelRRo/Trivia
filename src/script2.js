5529600290879875c8c004ee93e7a74361ecfd2f
Manuel Rico <ds23906@gmail.com>
2021-05-13 12:33:21 -0600

    color and fonts changes

--- src/script2.js
+++ src/script2.js
@@ -3,7 +3,7 @@
 
 const btnLogin = document.getElementById('btnLogin');
 
-const btnTopic = document.querySelectorAll('.topic');
+const btnTopic = document.querySelectorAll('.btn-topic');
 
 const btnAnswer = document.querySelectorAll('.buttonGame');
 
@@ -37,17 +37,17 @@ let listOfQuestion = [];
 let listOfAnswer = [];
 let listOfRightAnswer = [];
 
-let bookQuestion = ["The book “Da Vinci Code,” was written by who?", "What was Stephen King’s first published novel?", "What title did Jane Austen originally give to the book that was eventually published as Pride and Prejudice?"];
-let sciencieQuestion = ["What is the nearest planet to the sun?","How many teeth does an adult human have?","On what part of your body would you find the pinna?"];
-let historyQuestion = ["In which year did Hitler commit suicide?"," One of the ancient world wonders, the “Hanging Gardens,” was found in which city?","In 1927, who became the first man to fly solo and non-stop across the Atlantic?"];
+const bookQuestion = ["The book “Da Vinci Code,” was written by who?", "What was Stephen King’s first published novel?", "What title did Jane Austen originally give to the book that was eventually published as Pride and Prejudice?"];
+const sciencieQuestion = ["What is the nearest planet to the sun?","How many teeth does an adult human have?","On what part of your body would you find the pinna?"];
+const historyQuestion = ["In which year did Hitler commit suicide?"," One of the ancient world wonders, the “Hanging Gardens,” was found in which city?","In 1927, who became the first man to fly solo and non-stop across the Atlantic?"];
 
-let bookRightAnswer = ["Dan Brown", "Carrie", "First Impressions"];
-let sciencieRightAnswer = ["Mercury","32","Ear"];
-let historyRightAnswer = ["1945","Babylon","Charles Lindbergh"];
+const bookRightAnswer = ["Dan Brown", "Carrie", "First Impressions"];
+const sciencieRightAnswer = ["Mercury","32","Ear"];
+const historyRightAnswer = ["1945","Babylon","Charles Lindbergh"];
 
-let bookAnswer = ["George Orwell", "J.K Rowling", "Sam Collins","Dan Brown", "It", "The Stand", "The Shinnig", "Carrie", "Emma","First Impressions", "Persuasion", " Lady Elizabeth"];
-let sciencieAnswer = ["Venus","Earth","Jupiter","Mercury","32","31","30","29","Leg","foot","Tongue","Ear"];
-let historyAnswer = ["1945","1939","1944","1941","Babylon","Damascus","Israel","Turkey","Charles Wick","River plate","Andrew Jackson","Charles Lindbergh"];
+const bookAnswer = ["George Orwell", "J.K Rowling", "Sam Collins","Dan Brown", "It", "The Stand", "The Shinnig", "Carrie", "Emma","First Impressions", "Persuasion", " Lady Elizabeth"];
+const sciencieAnswer = ["Venus","Earth","Jupiter","Mercury","32","31","30","29","Leg","foot","Tongue","Ear"];
+const historyAnswer = ["1945","1939","1944","1941","Babylon","Damascus","Israel","Turkey","Charles Wick","River plate","Andrew Jackson","Charles Lindbergh"];
 
 
 let number_question = 0;
@@ -61,7 +61,7 @@ addClass(loginContainer,"show");
 btnLogin.addEventListener("click",function () {
   removeClass(loginContainer,"show");
   addClass(topicContainer,"show");
-  nickName.innerText = userName.value;
+  showNickname();
 });
 
 btnTopic.forEach(btn => {
@@ -70,40 +70,44 @@ btnTopic.forEach(btn => {
     addClass(answerQuestionContainer,"show");
     setupQuestionAnswer(btn);
     showQuestion();
-    btnAnswer.forEach(btn__ => {setAnswer(btn__,listOfAnswer);});
+    btnAnswer.forEach(btn__ => {showAnswer(btn__); });
   });
 });
 
 btnAnswer.forEach(btn => {
-  setAnswer(btn,listOfAnswer);
+  showAnswer(btn);
   btn.addEventListener("click",function () {
     showQuestion();
     addPoints(getUserAnswer(btn));
-    btnAnswer.forEach(btn__ => {setAnswer(btn__,listOfAnswer);});
-
-    if(questionsShowed === 2){
-      answerQuestionContainer.classList.remove("show");
-      scoreContainer.classList.add("show");
-    }
-    questionsShowed++;
+    showFinalScore(questionsShowed);
+    btnAnswer.forEach(btn__ => {showAnswer(btn__); });
   });
 });
 
+function showFinalScore (){
+
+  if(questionsShowed === 2){
+    addClass(scoreContainer,"show");
+    removeClass(answerQuestionContainer,"show"); 
+  }
+
+  questionsShowed++;
+
+}
+
 btnReturn.addEventListener('click',function () {
-  scoreContainer.classList.remove("show");
-  topicContainer.classList.add("show");
+  addClass(topicContainer,"show");
+  removeClass(scoreContainer,"show"); 
   emptyGlobalScore();
   emptyQASection();
-  console.log(listOfQuestion);
 });
 
 btnNewGame.addEventListener('click', function () {
-  scoreContainer.classList.remove("show");
-  loginContainer.classList.add("show");
+  addClass(loginContainer,"show");
+  removeClass(scoreContainer,"show"); 
   emptyGlobalScore();
   emptyQASection();
-  nickName.innerText = "";
-  userName.value = "";
+  resetLoginData();
 });
 
 function emptyQASection(){
@@ -154,13 +158,25 @@ function showQuestion () {
   }
 }
 
-function setAnswer (__btn,__answer) {
+function setAnswer (__answer) {
+
+  let str = "";
+
   if(number_answer < __answer.length) {
 
-    __btn.innerText = __answer[number_answer];
+    str = __answer[number_answer];
 
     number_answer++;
+    
   }
+
+  return str;
+}
+
+function showAnswer (btn) {
+
+  btn.innerText = setAnswer(listOfAnswer); 
+
 }
 
 function buildListOfQuestion (idTopic_) {
@@ -218,4 +234,13 @@ function setupQuestionAnswer (btn) {
   listOfAnswer = buildListOfAnswer(idTopic);
   listOfRightAnswer = buildListOfRightAnswers(idTopic);
 
+}
+
+function showNickname () {
+  nickName.innerText = userName.value;
+}
+
+function resetLoginData () {
+  nickName.innerText = "";
+  userName.value = "";
 }
